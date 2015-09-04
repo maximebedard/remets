fb5ad0564bf7b7d54807c5be0776394f72a20ece
Maxime Bedard <maxime.bedard@shopify.com>
2015-09-04 07:26:40 -0400

    Start writing specs for handover

--- test/controllers/handovers_controller_test.rb
+++ test/controllers/handovers_controller_test.rb
@@ -5,12 +5,30 @@ class HandoversControllerTest < ActionController::TestCase
     @handover = handovers(:log121_lab1)
   end
 
-  test '#index'
-  test '#show'
-  test '#new'
-  test '#create'
-  test '#edit'
-  test '#update'
+  test '#index' do
+    get :index
+    assert_response :ok
+  end
+
+  test '#show' do
+    get :show, id: @handover.id
+    assert_response :ok
+  end
+
+  test '#new' do
+    get :new
+    assert_response :ok
+  end
+
+  test '#create' do
+  end
+
+  test '#edit' do
+  end
+
+  test '#update' do
+  end
+
 
   test '#index with json format'
   test '#show with json format'
