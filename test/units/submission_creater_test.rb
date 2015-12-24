22653b1c332a5eb5b432f3b14d28ca9384d91a01
Maxime Bedard <maxime.bedard@shopify.com>
2015-12-23 22:17:20 -0500

    Fix the remaining tests

--- test/units/submission_creater_test.rb
+++ test/units/submission_creater_test.rb
@@ -3,29 +3,37 @@ require "test_helper"
 class SubmissionCreaterTest < ActiveSupport::TestCase
   include Remets::DocumentFileUploadHelper
 
-  setup do
-    @fixture = sanitizable_file_upload
-    @service = SubmissionCreater.new(
-      documents_attributes: [{ file_ptr: @fixture }],
-    )
+  test "#perform creates a submission with a single document" do
+    assert_difference(["Submission.count", "Document.count"]) do
+      perform(files: [sanitizable_file_upload])
+    end
   end
 
-  test "#perform creates a submission with a single document" do
-    assert_difference ["Submission.count", "Document.count"] do
-      @service.perform
+  test "#perform fingerprints a single document content" do
+    assert_enqueued_jobs(1) do
+      perform(files: [sanitizable_file_upload])
     end
   end
 
   test "#perform creates a submission with multiple documents" do
-  end
-
-  test "#perform fingerprints a single document content" do
-    submission = @service.perform.reload
-    document = submission.documents.first
-
-    assert_not_empty document.windows
+    assert_difference("Submission.count") do
+      assert_difference("Document.count", 2) do
+        perform(files: [sanitizable_file_upload] * 2)
+      end
+    end
   end
 
   test "#perform fingerprints multiple documents content" do
+    assert_enqueued_jobs(2) do
+      perform(files: [sanitizable_file_upload] * 2)
+    end
+  end
+
+  private
+
+  def perform(files: [])
+    SubmissionCreater.new(
+      documents_attributes: files.map { |f| { file_ptr: f } },
+    ).perform
   end
 end
