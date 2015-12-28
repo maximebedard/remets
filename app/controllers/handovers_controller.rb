757d5696d8c2e9d1212876c7a9ddaa682dc130f1
Maxime Bedard <maxime.bedard@shopify.com>
2015-12-28 14:55:00 -0500

    Handover controller take 2

--- app/controllers/handovers_controller.rb
+++ app/controllers/handovers_controller.rb
@@ -5,4 +5,37 @@ class HandoversController < ApplicationController
     @handovers = Handover.all
     respond_with(@handovers)
   end
+
+  def edit
+    @handover = Handover.find(params[:id])
+    respond_with(@handover)
+  end
+
+  def update
+    @handover = Fingerprinter.new(
+      Handover.find(params[:id]),
+      handover_params,
+    ).call
+    respond_with(@handover)
+  end
+
+  def new
+    @handover = Handover.new
+    respond_with(@handover)
+  end
+
+  def create
+    @handover = Fingerprinter.new(
+      Handover.new,
+      handover_params,
+    ).call
+    respond_with(@handover)
+  end
+
+  private
+
+  def handover_params
+    params.require(:handover)
+      .permit(:name, documents_attributes: [:file_ptr])
+  end
 end
--- test/controllers/handovers_controller_test.rb
+++ test/controllers/handovers_controller_test.rb
@@ -1,4 +1,56 @@
 require "test_helper"
 
 class HandoversControllerTest < ActionController::TestCase
+  include Remets::DocumentFileUploadHelper
+  setup do
+    @handover = handovers(:log121_lab1)
+  end
+
+  test "#index" do
+    get :index
+
+    assert assigns(:handovers)
+    assert_response :ok
+  end
+
+  test "#edit" do
+    get :edit, id: @handover.id
+
+    assert assigns(:handover)
+    assert_response :ok
+  end
+
+  test "#update" do
+    file = sanitizable_file_upload
+    patch(
+      :update,
+      id: @handover.id,
+      handover: {
+        name: "pants",
+        documents_attributes: [{ file_ptr: file }],
+      },
+    )
+    assert_redirected_to assigns(:handover)
+  end
+
+  test "#new" do
+    get :new
+
+    assert assigns(:handover)
+    assert_response :ok
+  end
+
+  test "#create" do
+    file = sanitizable_file_upload
+    post(
+      :create,
+      handover: {
+        name: "pants",
+        documents_attributes: [
+          { file_ptr: file },
+        ],
+      },
+    )
+    assert_redirected_to assigns(:handover)
+  end
 end
