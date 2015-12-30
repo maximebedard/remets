781d9d9547b07c961b47ab3e9049194b21006375
Maxime Bedard <maxime.bedard@shopify.com>
2015-12-30 13:44:05 -0500

    Add application controller tests

---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,45 @@
+require "test_helper"
+
+class ApplicationControllerTest < ActionController::TestCase
+  test "#current_user returns the user from the session" do
+    @controller.session[Remets::AUTH_SESSION_KEY] = users(:gaston)
+
+    assert_equal "rinfrette.gaston@gmail.com", @controller.current_user.email
+  end
+
+  test "#current_user returns nil when the session is not present" do
+    @controller.session.delete(Remets::AUTH_SESSION_KEY)
+
+    assert_nil @controller.current_user
+  end
+
+  test "#current_user= sets the user in the session" do
+    @controller.current_user = users(:gaston)
+
+    assert_equal "rinfrette.gaston@gmail.com", @controller.current_user.email
+  end
+
+  test "signed_in? is true when the current_user is present" do
+    @controller.current_user = users(:gaston)
+
+    assert @controller.signed_in?
+  end
+
+  test "signed_in? is false when the current_user is blank" do
+    @controller.current_user = nil
+
+    refute @controller.signed_in?
+  end
+
+  test "signed_out? is true when the current_user is blank" do
+    @controller.current_user = nil
+
+    assert @controller.signed_out?
+  end
+
+  test "signed_out? is false when the current_user is present" do
+    @controller.current_user = users(:gaston)
+
+    refute @controller.signed_out?
+  end
+end
