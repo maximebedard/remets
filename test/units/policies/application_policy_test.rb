323a0f2c31285dd7cba520373b31f6a0632c0d57
Maxime Bedard <maxime.bedard@shopify.com>
2015-12-31 01:29:11 -0500

    Add application policy tests

--- test/units/policies/application_policy_test.rb
+++ test/units/policies/application_policy_test.rb
@@ -1,4 +1,44 @@
 require "test_helper"
 
 class ApplicationPolicyTest < ActiveSupport::TestCase
+  include Remets::PolicyAssertions
+
+  setup do
+    @policy = ApplicationPolicy.new(
+      users(:henry),
+      documents(:platypus),
+    )
+  end
+
+  test "#index?" do
+    refute @policy.index?
+  end
+
+  test "#show?" do
+    assert @policy.show?
+  end
+
+  test "#create?" do
+    refute @policy.create?
+  end
+
+  test "#new?" do
+    refute @policy.new?
+  end
+
+  test "#update?" do
+    refute @policy.update?
+  end
+
+  test "#edit?" do
+    refute @policy.edit?
+  end
+
+  test "#destroy" do
+    refute @policy.destroy?
+  end
+
+  test "#scope" do
+    assert_equal Document, @policy.scope
+  end
 end
