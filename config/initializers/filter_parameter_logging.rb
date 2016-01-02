7c3dc1edee9fd6597daf108eadf49126850af9bd
Maxime Bedard <maxime.bedard@shopify.com>
2016-01-01 19:34:00 -0500

    Add log truncate

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,3 +2,19 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [:password]
+
+module LogWithBinaryTruncate
+  protected
+
+  TRUNCATE_LENGTH = 100
+  def log(sql, name = "SQL", binds = [], statement_name = nil, &block)
+    b = binds.map do |k, v|
+      v = v.truncate(TRUNCATE_LENGTH) if v.is_a?(String) && v.size > TRUNCATE_LENGTH
+      [k, v]
+    end
+
+    super(sql, name, b, statement_name, &block)
+  end
+end
+
+ActiveRecord::ConnectionAdapters::AbstractAdapter.prepend(LogWithBinaryTruncate)
