2af3227fbc08c60f9e9fb0e35b752008cb4887bb
Maxime Bedard <maxime.bedard@shopify.com>
2016-01-03 13:41:29 -0500

    Update indexing job

--- app/models/document_match.rb
+++ app/models/document_match.rb
@@ -3,6 +3,8 @@ class DocumentMatch < ActiveRecord::Base
   belongs_to :compared_document, class_name: Document
   belongs_to :match, inverse_of: :document_matches
 
+  delegate :fingerprints, to: :match
+
   validates(
     :reference_document_id,
     :compared_document_id,
