55036d8759fff197e76fea6c911b906084aa9d3f
Maxime Bedard <maxime.bedard@shopify.com>
2016-01-03 13:15:21 -0500

    Add Match model to minimize dupplication

---
+++ app/models/match.rb
@@ -0,0 +1,4 @@
+class Match < ActiveRecord::Base
+  has_many :document_matches
+  validates :fingerprints, presence: true
+end
--- test/units/models/document_match_test.rb
+++ test/units/models/document_match_test.rb
@@ -10,7 +10,7 @@ class DocumentMatchTest < ActiveSupport::TestCase
     document1.windows = [[0, 1234], [1, 5678]]
     document2.windows = [[0, 4321], [1, 8765]]
 
-    assert_no_difference("DocumentMatch.count") do
+    assert_no_difference("DocumentMatch.count", "Match.count") do
       DocumentMatch.create_from!(document1, document2)
     end
   end
@@ -22,29 +22,38 @@ class DocumentMatchTest < ActiveSupport::TestCase
     document1.windows = [[0, 1234], [1, 5678]]
     document2.windows = [[9, 1234], [1, 8765]]
 
-    assert_difference("DocumentMatch.count") do
-      DocumentMatch.create_from!(document1, document2)
+    assert_difference("DocumentMatch.count", 2) do
+      assert_difference("Match.count") do
+        DocumentMatch.create_from!(document1, document2)
+      end
     end
   end
 
   test "#relevant_matches return the matches for the compared document" do
     reference = documents(:platypus)
     DocumentMatch.destroy_all
-    DocumentMatch.create(
+    Match.destroy_all
+
+    match1 = Match.create!(fingerprints: [1234, 5678, 0000])
+    match2 = Match.create!(fingerprints: [1234, 5678])
+
+    DocumentMatch.create!(
       reference_document: reference,
       compared_document: documents(:fraudulent_platypus),
-      fingerprints: [1234, 5678, 0000],
+      match: match1,
     )
-    DocumentMatch.create(
+    DocumentMatch.create!(
       reference_document: reference,
       compared_document: documents(:fraudulent_bragging),
-      fingerprints: [1234, 5678],
-    )
-    DocumentMatch.create(
-      reference_document: reference,
-      compared_document: documents(:empty),
+      match: match2,
     )
 
-    assert_equal 2, DocumentMatch.relevant_matches(reference).size
+    relevant_matches = DocumentMatch.relevant_matches(reference)
+    assert_equal 2, relevant_matches.size
+    assert_equal documents(:fraudulent_platypus).id,
+      relevant_matches.first.compared_document_id
+    assert_equal documents(:fraudulent_bragging).id,
+      relevant_matches.second.compared_document_id
+    assert relevant_matches.all? { |m| m.reference_document_id == reference.id }
   end
 end
