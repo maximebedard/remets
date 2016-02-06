28922a414a960bbe4cf160e08f550eb9684c3da2
Xavier Drdak <x.drdak@gmail.com>
2016-02-06 13:24:22 -0500

    this is broken lol

--- db/migrate/20150903033512_create_handover.rb
+++ db/migrate/20150903033512_create_handover.rb
@@ -1,3 +1,4 @@
+require SecureRandom
 class CreateHandover < ActiveRecord::Migration
   def change
     create_table :handovers do |t|
@@ -5,6 +6,9 @@ class CreateHandover < ActiveRecord::Migration
       t.string :title
       t.text :description
       t.timestamps null: false
+      t.uuid :uuid, unique: true
+      t.boolean :invite_only, default: true
+      t.string :password, null: true
     end
   end
 end
