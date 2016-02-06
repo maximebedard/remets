2eb37a7685dab68cfbaf361fedd68254c18228dd
Maxime Bedard <maxime.bedard@shopify.com>
2016-02-06 13:55:42 -0500

    Simplify application helper test

--- test/units/helpers/application_helper_test.rb
+++ test/units/helpers/application_helper_test.rb
@@ -1,16 +1,11 @@
 require "test_helper"
 
-class ApplicationHelperTest < ActiveSupport::TestCase
-  include ApplicationHelper
-  include ActionView::TestCase::Behavior
-
+class ApplicationHelperTest < ActionView::TestCase
   test "#active_on_helper returns active when controller name matches user input" do
-    setup_with_controller
     assert_equal "active", active_on_controller(@controller.controller_name)
   end
 
   test "#active_on_helper returns nil when controller name does not match user input" do
-    setup_with_controller
-    assert_equal nil, active_on_controller(@controller.controller_name + "_NOPE")
+    assert_equal nil, active_on_controller("yolo")
   end
 end
