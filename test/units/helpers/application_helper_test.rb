befb10a9b25c7eefd209855d969052808c654dc6
Maxime Bedard <maxime.bedard@shopify.com>
2016-02-07 23:15:17 -0500

    Remove white-bg helper since we use a layout

--- test/units/helpers/application_helper_test.rb
+++ test/units/helpers/application_helper_test.rb
@@ -8,14 +8,4 @@ class ApplicationHelperTest < ActionView::TestCase
   test "#active_on_controller returns nil when controller name does not match user input" do
     assert_equal nil, active_on_controller("yolo")
   end
-
-  test "#white_background returns white-bg when controller name equals home" do
-    @controller.stubs(controller_name: "home")
-    assert_equal "white-bg", white_background
-  end
-
-  test "#white_background returns nil when controller name does not equal home" do
-    @controller.stubs(controller_name: "yolo")
-    assert_equal nil, white_background
-  end
 end
