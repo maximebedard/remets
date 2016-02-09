c3fd5b879d2f7f87394dd7932f628898cbd6459d
Maxime Bedard <maxime.bedard@shopify.com>
2016-02-08 21:08:20 -0500

    Add missing submission policy tests

--- test/units/policies/organization_policy_test.rb
+++ test/units/policies/organization_policy_test.rb
@@ -40,26 +40,26 @@ class OrganizationPolicyTest < ActiveSupport::TestCase
   end
 
   test "#index?" do
-    assert_permit(users(:henry), :account, :index)
+    assert_permit(users(:henry), organizations(:ets), :index)
   end
 
   test "#show?" do
-    assert_permit(users(:henry), :account, :show)
+    assert_permit(users(:henry), organizations(:ets), :show)
   end
 
   test "#edit?" do
-    assert_permit(users(:henry), :account, :edit)
+    assert_permit(users(:henry), organizations(:ets), :edit)
   end
 
   test "#update?" do
-    assert_permit(users(:henry), :account, :update)
+    assert_permit(users(:henry), organizations(:ets), :update)
   end
 
   test "#new?" do
-    assert_permit(users(:henry), :account, :new)
+    assert_permit(users(:henry), organizations(:ets), :new)
   end
 
   test "#create?" do
-    assert_permit(users(:henry), :account, :create)
+    assert_permit(users(:henry), organizations(:ets), :create)
   end
 end
