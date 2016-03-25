5db9d6e4e0b646a85e7973467fd0d39e9205eb75
Maxime Bedard <maxime.bedard@shopify.com>
2016-03-25 15:57:17 -0700

    Rename active_on_controller => active_for

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def active_on_controller(input)
+  def active_for(input)
     "active" if controller.controller_name == input
   end
 end
--- test/units/helpers/application_helper_test.rb
+++ test/units/helpers/application_helper_test.rb
@@ -1,11 +1,11 @@
 require "test_helper"
 
 class ApplicationHelperTest < ActionView::TestCase
-  test "#active_on_controller returns active when controller name matches user input" do
-    assert_equal "active", active_on_controller(@controller.controller_name)
+  test "#active_for returns active when controller name matches user input" do
+    assert_equal "active", active_for(@controller.controller_name)
   end
 
-  test "#active_on_controller returns nil when controller name does not match user input" do
-    assert_equal nil, active_on_controller("yolo")
+  test "#active_for returns nil when controller name does not match user input" do
+    assert_equal nil, active_for("yolo")
   end
 end
