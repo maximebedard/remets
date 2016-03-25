3fa49a64f52c018b62c93f5dea277843eddad25a
Maxime Bedard <maxime.bedard@shopify.com>
2016-03-25 15:57:42 -0700

    Rename param input => name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def active_for(input)
-    "active" if controller.controller_name == input
+  def active_for(name)
+    "active" if controller.controller_name == name
   end
 end
