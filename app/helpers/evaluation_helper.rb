31c4f5f33c661e6f67ad306ad44eda26b69f3e07
Maxime Bedard <maxime.bedard@shopify.com>
2016-03-31 22:24:49 -0400

    PATCH only when the evaluation is persisted

--- app/helpers/evaluation_helper.rb
+++ app/helpers/evaluation_helper.rb
@@ -1,6 +1,6 @@
 module EvaluationHelper
   def create_or_update_evaluation_path(evaluation)
-    return evaluation_path(uuid: evaluation.uuid) if evaluation.uuid.present?
+    return evaluation_path(uuid: evaluation.uuid) if evaluation.persisted?
     evaluations_path
   end
 end
