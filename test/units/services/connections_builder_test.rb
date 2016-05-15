090049e4fe06a9f2221a613de3b8b6926d2ca4bb
Maxime Bedard <maxime.bedard@shopify.com>
2016-05-15 11:25:56 -0400

    Fix broken test for connection builder

--- test/units/services/connections_builder_test.rb
+++ test/units/services/connections_builder_test.rb
@@ -4,29 +4,28 @@ class ConnectionsBuilderTest < ActiveSupport::TestCase
   setup do
     @organization = organizations(:ets)
     @provider = ConnectionsProviders::MembershipProvider.new
-  end
-
-  test "#call replaces the members" do
-    ConnectionsBuilder.new(
+    @service = ConnectionsBuilder.new(
       @organization,
       [users(:henry).email, users(:clement).email],
       provider: @provider,
-    ).call
+    )
+  end
 
-    assert_equal 3, @organization.memberships.size
-    assert_equal ["clement.bisson@gmail.com", "lemieux.henry@gmail.com", "rinfrette.gaston@gmail.com"],
+  test "#call replaces the members" do
+    @organization.memberships = @service.call
+
+    assert_equal 2, @organization.memberships.size
+    assert_equal ["clement.bisson@gmail.com", "lemieux.henry@gmail.com"],
       @organization.users.pluck(:email).sort
   end
 
   test "#call append the organization owner" do
-    ConnectionsBuilder.new(
-      @organization,
-      [],
-      provider: @provider,
-    ).call(include_owner: true)
+    @organization.memberships = @service.call(
+      include_owner: true,
+    )
 
-    assert_equal 1, @organization.memberships.size
-    assert_equal "rinfrette.gaston@gmail.com",
-      @organization.users.first.email
+    assert_equal 3, @organization.memberships.size
+    assert_equal ["clement.bisson@gmail.com", "lemieux.henry@gmail.com", "rinfrette.gaston@gmail.com"],
+      @organization.users.pluck(:email).sort
   end
 end
